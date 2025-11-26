399c2f938eef1ffc11b93da4655a12021e260d9c
fatkharrofiqi <achmadfatkharrofiqi404@gmail.com>
2025-11-26 07:03:08 +0700

    feat: implement account management API and configuration loading

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -6,12 +6,8 @@ import (
 	"os"
 	"testing"
 
+	"github.com/fatkharrofiqi/simplebank/utils"
 	_ "github.com/lib/pq"
-)
-
-const (
-	dbDriver = "postgres"
-	dbSource = "postgresql://root:root@localhost:5432/simple_bank?sslmode=disable"
 )
 
 var testQueries *Queries
@@ -19,7 +15,11 @@ var testDB *sql.DB
 
 func TestMain(m *testing.M) {
 	var err error
-	testDB, err = sql.Open(dbDriver, dbSource)
+	config, err := utils.LoadConfig("../..")
+	if err != nil {
+		log.Fatal("cannot load config:", err)
+	}
+	testDB, err = sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
 	}
