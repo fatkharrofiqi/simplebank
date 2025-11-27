7690fb76f3072a38fd749c6acc0e06702e0e8aff
fatkharrofiqi <achmadfatkharrofiqi404@gmail.com>
2025-11-27 16:10:42 +0700

    feat: implement transfer functionality and currency validation

---
+++ utils/currency.go
@@ -0,0 +1,16 @@
+package utils
+
+// Constanst for all supported currencies
+const (
+	USD = "USD"
+	EUR = "EUR"
+	CAD = "CAD"
+)
+
+func IsSupportedCurrency(currency string) bool {
+	switch currency {
+	case USD, CAD, EUR:
+		return true
+	}
+	return false
+}
